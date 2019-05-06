a94c8d60a682dcd23fb2d99ef15610ca5619b943
nmovellan <jimovellan@gmail.com>
2019-05-06 05:42:46 +0200

    cambio en repositorio base

---
+++ Application.Frontal/Scripts/App/App.gasto.js
@@ -0,0 +1,39 @@
+﻿App.gasto = App.gasto || {};
+
+App.gasto.CargarTabla = function(dataTableName){
+
+
+        var datatable = $("#" + dataTableName);
+        var urlajax = datatable.attr["data-ajax"];
+        $("#" + dataTableName).DataTable({
+        	"ajax": urlajax,
+        	dataSrc: '',
+        	select: true,
+        	columns: [
+                { data: 'Descripcion' },
+                { data: 'Fecha' },
+				{ data: 'Importe' },
+				{data:'TipoGasto.Descripcion'},
+
+                {
+                	sortable: false,
+                	"render": function (data, type, full, meta) {
+                		var buttonID = full.tipogasto;
+                		return "<a class= 'btn btn-danger glyphicon glyphicon-remove' role='button' onclick='App.tipogasto.eliminarTipo(" + full.TipoGastoID + ")'></a>" +
+                            "<a class= 'btn btn-success glyphicon glyphicon-edit' role='button' onclick='App.tipogasto.nuevoTipo(" + full.TipoGastoID + ")'></a>";
+
+
+
+
+                	}
+                },
+        	]
+        });
+        
+
+};
+
+$(document).ready(function () {
+	App.gasto.CargarTabla('tablagastos')
+
+});
